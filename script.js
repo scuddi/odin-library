1ae6071499c7dbe259d617973b75bda0708205d7
Konstantin <157764402+scuddi@users.noreply.github.com>
2025-03-21 10:20:56 +0100

    Add basic javascript code for creating books

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function Books(title, author, pages, read) {
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-       return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} yet.`;
-    };
+};
+
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read");
+
+const myLibrary = [];
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+};
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        // Here comes all the text for the display.
+    }
 }
-
-const theHobbit = new Books("The Hobbit", "J.R.R. Tolkien", 295, "not read");
-
-theHobbit.info();
