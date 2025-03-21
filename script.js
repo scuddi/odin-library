7237f42505e3a61bf7aef49b6dba6c0caa89782c
Konstantin <157764402+scuddi@users.noreply.github.com>
2025-03-21 16:17:13 +0100

    Add functionality to displayBooks function

--- script.js
+++ script.js
@@ -10,15 +10,42 @@ const myLibrary = [];
 
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
+    newBook.id = crypto.randomUUID();
     myLibrary.push(newBook);
 };
 
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
-        //...
-    }
+        content = document.getElementById("content");
+
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+
+        
+        titleDiv = document.createElement("div");
+        titleDiv.classList.add("book-title");
+        titleDiv.innerHTML = `"${myLibrary[i].title}"`;
+
+        authorDiv = document.createElement("div");
+        authorDiv.classList.add("book-author");
+        authorDiv.innerHTML = `Author: ${myLibrary[i].author}`;
+        
+        pagesDiv = document.createElement("div");
+        pagesDiv.classList.add("book-pages");
+        pagesDiv.innerHTML = `Pages: ${myLibrary[i].pages}`;
+        
+        readDiv = document.createElement("div");
+        readDiv.classList.add("book-read");
+        readDiv.innerHTML = `Status: ${myLibrary[i].read}`;
+
+
+        content.appendChild(bookCard);
+        bookCard.append(titleDiv, authorDiv, pagesDiv, readDiv)
+    };
 };
 
 addBookToLibrary("The Phoenix Project", "Gene Kim", 345, "read");
 addBookToLibrary("The Abolition of Man", "C. S. Lewis", 48, "not read");
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read");
+
+displayBooks(myLibrary);
